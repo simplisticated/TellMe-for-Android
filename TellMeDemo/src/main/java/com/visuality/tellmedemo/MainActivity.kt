01cb840a743d900cef752d30ebc7dac7e296977b
Igor Matyushkin <igormatyushkin014@gmail.com>
2019-03-28 00:03:33 +0300

    Added project

---
+++ TellMeDemo/src/main/java/com/visuality/tellmedemo/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.visuality.tellmedemo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
