830822498ddf673c7b7d6c474ea80389d173c790
Igor Matyushkin <igormatyushkin014@gmail.com>
2019-03-28 02:34:42 +0300

    Implemented Speaker

--- TellMe/src/main/java/com/visuality/tellme/engine/Speaker.kt
+++ TellMe/src/main/java/com/visuality/tellme/engine/Speaker.kt
@@ -1,9 +1,12 @@
 package com.visuality.tellme.engine
 
 import android.content.Context
+import android.os.Handler
+import android.os.Looper
 import android.speech.tts.TextToSpeech
 import android.speech.tts.UtteranceProgressListener
 import java.util.*
+import kotlin.properties.Delegates
 
 class Speaker(
     private val context: Context,
@@ -17,33 +20,34 @@ class Speaker(
         }
     )
 
+    private var textToSpeechInitialized = false
+
     private var speechQueue = arrayListOf<Speech>()
 
-    private var lastSpeechId = 0
+    private var shouldDestroyWhenQueueBecomesEmpty = false
 
-    private var onReadyHandler: OnSpeakerReadyHandler? = null
-
-    fun whenReady(handler: OnSpeakerReadyHandler): Speaker {
-        this.onReadyHandler = handler
+    fun destroyWhenFinish(): Speaker {
+        this.shouldDestroyWhenQueueBecomesEmpty = true
         return this
     }
 
-    private var onProgressHandler: OnSpeakerProgressHandler? = null
+    private var onInitListener: OnInitListener? = null
 
-    fun onProgress(handler: OnSpeakerProgressHandler): Speaker {
-        this.onProgressHandler = handler
+    fun setOnInitListener(listener: OnInitListener?): Speaker {
+        this.onInitListener = listener
         return this
     }
 
-    private var onFinishedHandler: OnSpeakerFinishedHandler? = null
+    private var onSpeechListener: OnSpeechListener? = null
 
-    fun onFinished(handler: OnSpeakerFinishedHandler): Speaker {
-        this.onFinishedHandler = handler
+    fun setOnSpeechListener(listener: OnSpeechListener?): Speaker {
+        this.onSpeechListener = listener
         return this
     }
 
     private fun onTextToSpeechInit(status: Int) {
         if (status != TextToSpeech.SUCCESS) {
+            this.onInitListener?.onFailed()
             return
         }
 
@@ -51,39 +55,68 @@ class Speaker(
         this.textToSpeech.setOnUtteranceProgressListener(
             object : UtteranceProgressListener() {
                 override fun onDone(utteranceId: String?) {
-                    val indexToRemove = this@Speaker.speechQueue.indexOfFirst { speech ->
-                        speech.id.toString().equals(utteranceId!!, true)
-                    }
+                    val speech = this@Speaker.getSpeechFromQueue(utteranceId!!) ?: return
+                    this@Speaker.speechQueue.remove(speech)
+                    this@Speaker.onSpeechListener?.onFinishedSaying(speech.text)
 
-                    if (0 <= indexToRemove && indexToRemove < this@Speaker.speechQueue.size) {
-                        this@Speaker.speechQueue.removeAt(indexToRemove)
+                    if (this@Speaker.speechQueue.isEmpty() && this@Speaker.shouldDestroyWhenQueueBecomesEmpty) {
+                        this@Speaker.close()
                     }
-
-                    this@Speaker.onFinishedHandler?.invoke(this@Speaker)
                 }
 
                 override fun onError(utteranceId: String?) {
                 }
 
                 override fun onStart(utteranceId: String?) {
+                    val speech = this@Speaker.getSpeechFromQueue(utteranceId!!) ?: return
+                    Handler(Looper.getMainLooper()).post {
+                        this@Speaker.onSpeechListener?.onStartedSaying(
+                            speech.text
+                        )
+                    }
                 }
 
                 override fun onRangeStart(utteranceId: String?, start: Int, end: Int, frame: Int) {
                     super.onRangeStart(utteranceId, start, end, frame)
-                    this@Speaker.onProgressHandler?.invoke(this@Speaker)
+
+                    val speech = this@Speaker.getSpeechFromQueue(utteranceId!!) ?: return
+                    val position = SpeechPosition(
+                        start = start,
+                        length = end - start + 1
+                    )
+                    this@Speaker.onSpeechListener?.onProgress(
+                        speech.text,
+                        position
+                    )
                 }
             }
         )
-        this.onReadyHandler?.invoke(this)
+
+        this.onInitListener?.onSuccess()
+
+        for (speech in this.speechQueue) {
+            this.pronounceSpeech(speech)
+        }
     }
 
-    private fun putTextOnQueue(text: String) {
-        this.lastSpeechId++
-        val speech = Speech(
+    private fun createSpeechWithText(text: String): Speech {
+        LAST_SPEECH_ID++
+        return Speech(
             text,
-            this.lastSpeechId
+            LAST_SPEECH_ID
         )
-        this.speechQueue.add(speech)
+    }
+
+    private fun getSpeechFromQueue(utteranceId: String): Speech? {
+        return this.speechQueue.firstOrNull { speech ->
+            speech.id.toString().equals(
+                other = utteranceId,
+                ignoreCase = true
+            )
+        }
+    }
+
+    private fun pronounceSpeech(speech: Speech) {
         this.textToSpeech.speak(
             speech.text,
             TextToSpeech.QUEUE_ADD,
@@ -92,27 +125,49 @@ class Speaker(
         )
     }
 
-    fun say(text: String) {
-        this.putTextOnQueue(text)
+    private fun putSpeechOnQueue(speech: Speech, pronounce: Boolean) {
+        this.speechQueue.add(speech)
+
+        if (pronounce) {
+            this.pronounceSpeech(speech)
+        }
     }
 
-    fun sayImmediately(text: String) {
+    fun say(text: String): Speaker {
+        val speech = this.createSpeechWithText(text)
+        this.putSpeechOnQueue(
+            speech = speech,
+            pronounce = this.textToSpeechInitialized
+        )
+        return this
+    }
+
+    fun stop() {
         this.textToSpeech.stop()
         this.speechQueue.clear()
-        this.putTextOnQueue(text)
     }
 
     fun close() {
-        this.onReadyHandler = null
-        this.onProgressHandler = null
-        this.onFinishedHandler = null
-        this.textToSpeech.stop()
-        this.textToSpeech.shutdown()
+        this.onInitListener = null
+        this.onSpeechListener = null
+        this.textToSpeech.apply {
+            stop()
+            shutdown()
+        }
+    }
+
+    internal companion object {
+        private var LAST_SPEECH_ID = 0
+    }
+
+    interface OnInitListener {
+        fun onSuccess()
+        fun onFailed()
+    }
+
+    interface OnSpeechListener {
+        fun onStartedSaying(text: String)
+        fun onProgress(text: String, position: SpeechPosition)
+        fun onFinishedSaying(text: String)
     }
 }
-
-typealias OnSpeakerReadyHandler = (speaker: Speaker) -> Unit
-
-typealias OnSpeakerProgressHandler = (speaker: Speaker) -> Unit
-
-typealias OnSpeakerFinishedHandler = (speaker: Speaker) -> Unit
