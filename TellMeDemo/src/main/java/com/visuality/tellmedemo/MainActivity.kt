1fb9397b21e14c70bf4fa1571120e2c88bfd8b32
Igor Matyushkin <igormatyushkin014@gmail.com>
2019-03-28 04:07:24 +0300

    Updated demo project

--- TellMeDemo/src/main/java/com/visuality/tellmedemo/MainActivity.kt
+++ TellMeDemo/src/main/java/com/visuality/tellmedemo/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.visuality.tellmedemo
 
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
